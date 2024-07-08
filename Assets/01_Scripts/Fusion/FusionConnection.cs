b2a2c47ce7897208606c4bce44aedd5a11a83c37
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-07-08 10:29:52 +0900

    Fusion

---
+++ Assets/01_Scripts/Fusion/FusionConnection.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System.Collections.Generic;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace Dicer.Fusion {
+    public class FusionConnection : MonoBehaviour, INetworkRunnerCallbacks
+    {
+        public enum ConnectionStatus
+        {
+            Disconnected,
+            Connecting,
+            Failed,
+            Connected,
+            Loading,
+            Loaded
+        }
+
+        public bool connectOnAwake = false;
+        [HideInInspector] public NetworkRunner _runner = null;
+        [SerializeField] private string _battleScenePath = "";
+
+        private void Awake()
+        {
+            if(connectOnAwake)
+            {
+                ConnectToRunner();
+            }
+        }
+
+        public async void ConnectToRunner()
+        {
+            if (_runner == null)
+            {
+                _runner = gameObject.AddComponent<NetworkRunner>();
+            }
+
+            NetworkSceneInfo scene = new NetworkSceneInfo();
+            scene.AddSceneRef(SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex));
+
+            var startGameArgs = new StartGameArgs()
+            {
+                GameMode = GameMode.Shared,
+                SessionName = "test",
+                Scene = scene,
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            };
+
+            // GameMode.Host = Start a session with a specific name
+            // GameMode.Client = Join a session with a specific name
+            await _runner.StartGame(startGameArgs);
+
+            if (_runner.IsSceneAuthority)
+            {
+                _runner.LoadScene(SceneRef.FromIndex(SceneUtility.GetBuildIndexByScenePath(_battleScenePath)), LoadSceneMode.Additive);
+            }
+        }
+
+        public void OnConnectedToServer(NetworkRunner runner)
+        {
+            Debug.Log("OnConnectedToServer");
+        }
+
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+        {
+        }
+
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+        {
+        }
+
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+        {
+        }
+
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+        {
+        }
+
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+        {
+        }
+
+        public void OnInput(NetworkRunner runner, NetworkInput input)
+        {
+        }
+
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+        {
+        }
+
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+        {
+        }
+
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+        {
+        }
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+        {
+            Debug.Log("OnPlayerJoined");
+        }
+
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+        {
+        }
+
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+        {
+        }
+
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+        {
+        }
+
+        public void OnSceneLoadDone(NetworkRunner runner)
+        {
+        }
+
+        public void OnSceneLoadStart(NetworkRunner runner)
+        {
+        }
+
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+        {
+        }
+
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+        {
+        }
+
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+        {
+        }
+    }
+}
+
