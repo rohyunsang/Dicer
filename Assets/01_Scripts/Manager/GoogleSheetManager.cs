46ac595928ed4eaf70bfa2919329ab861e340fff
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-07-08 12:35:28 +0900

    Update GoogleSheetManager.cs

--- Assets/01_Scripts/Manager/GoogleSheetManager.cs
+++ Assets/01_Scripts/Manager/GoogleSheetManager.cs
@@ -1,124 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.TextCore.Text;
 
-public class GoogleSheetManager : MonoBehaviour
+public static class GoogleSheetManager
 {
-    #region SingleTon Pattern
-    public static GoogleSheetManager Instance { get; private set; }
-    private void Awake()
+    public static List<UnitData> unitDatas = new List<UnitData>();
+
+    // 링크 뒤 edit ~ 부분을 빼고 export?format=tsv 추가하기
+    // origin url https://docs.google.com/spreadsheets/d/1Z_awcApNOODEqu---j7VFGDjjuyU5cl7IMinMXKHPII/edit?gid=0#gid=0
+    private const string unitDataURL = "https://docs.google.com/spreadsheets/d/1Z_awcApNOODEqu---j7VFGDjjuyU5cl7IMinMXKHPII/export?format=tsv"; 
+    public static IEnumerator Loader()
     {
-        // If an instance already exists and it's not this one, destroy this one
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this.gameObject);
-            return;
-        }
-
-        // Set this as the instance and ensure it persists across scenes
-        Instance = this;
-        DontDestroyOnLoad(this.gameObject);
-
-        // Initialize other components or variables if needed
-    }
-    #endregion
-
-    // 링크 뒤 export ~ 부분을 빼고 export?format=tsv 추가하기
-    const string slimeDataURL = "https://docs.google.com/spreadsheets/d/1ThIRLXCaoj25XtKjm6mFWOKnF3pqoViRYx8E-IXKGBM/export?format=tsv"; // Slime
-    const string enemyDataURL = "https://docs.google.com/spreadsheets/d/14DXsg4yP4Y9zS7Le3GI-szxJ76Loj0xBkGyt_A5w_8c/export?format=tsv"; //Enemy
-
-    [SerializeField]
-    public List<Slime> slimes = new List<Slime>();
-
-    [SerializeField]
-    public List<Enemy> enemys = new List<Enemy>();
-
-    IEnumerator Start()
-    {
-        //Slime
-        UnityWebRequest www = UnityWebRequest.Get(slimeDataURL);
+        UnityWebRequest www = UnityWebRequest.Get(unitDataURL);
         yield return www.SendWebRequest();
 
         string data = www.downloadHandler.text;
-        print(data);
-        ParseSlimeData(data);
+        Debug.Log(data);
 
-        // Enemy
-        www = UnityWebRequest.Get(enemyDataURL);
-        yield return www.SendWebRequest();
-
-        string enemyData = www.downloadHandler.text;
-        print(enemyData);
-        ParseEnemyData(enemyData);
-
+        ParseUnitData(data);
     }
-    void ParseSlimeData(string data)
+
+    
+    private static void ParseUnitData(string data)
     {
         string[] lines = data.Split('\n');
-        for (int i = 1; i < lines.Length; i++) // 첫 번째 줄은 헤더이므로 건너뜁니다.
+        for (int i = 1; i < lines.Length; i++) // first line is data type
         {
             string[] fields = lines[i].Split('\t');
-            if (fields.Length >= 12) // 필드가 충분한지 확인
+            if (fields.Length >= 13) // enough field checking 
             {
-                Slime slime = new Slime()
+                UnitData unitData = new UnitData()
                 {
                     Index = int.Parse(fields[0]),
                     Name = fields[1],
                     Grade = int.Parse(fields[2]),
                     HP = float.Parse(fields[3]),
-                    AttackDamage = float.Parse(fields[4]),
-                    Defense = float.Parse(fields[5]),
-                    AttackSpeed = float.Parse(fields[6]),
-                    MoveSpeed = float.Parse(fields[7]),
-                    Class = int.Parse(fields[8]),
-                    AttackRange = float.Parse(fields[9]),
-                    Cost = int.Parse(fields[10]),
-                    KRName = fields[11],
-                    DesText = fields[12],
+                    MP = float.Parse(fields[4]),
+                    AttackPower = float.Parse(fields[5]),
+                    Defense = float.Parse(fields[6]),
+                    AttackSpeed = float.Parse(fields[7]),
+                    MoveSpeed = float.Parse(fields[8]),
+                    Class = int.Parse(fields[9]),
+                    AttackRange = float.Parse(fields[10]),
+                    Cost = int.Parse(fields[11]),
+                    KRName = fields[12],
+                    DesText = fields[13],
                 };
-                slimes.Add(slime);
+                unitDatas.Add(unitData);
             }
         }
     }
-    void ParseEnemyData(string data)
-    {
-        string[] lines = data.Split('\n');
-        for (int i = 1; i < lines.Length; i++) // 첫 번째 줄은 헤더이므로 건너뜁니다.
-        {
-            string[] fields = lines[i].Split('\t');
-            if (fields.Length >= 10) // 필드가 충분한지 확인
-            {
-                Enemy enemy = new Enemy()
-                {
-                    Index = int.Parse(fields[0]),
-                    Name = fields[1],
-                    HP = float.Parse(fields[2]),
-                    AttackDamage = float.Parse(fields[3]),
-                    Defense = float.Parse(fields[4]),
-                    AttackSpeed = float.Parse(fields[5]),
-                    MoveSpeed = float.Parse(fields[6]),
-                    Class = int.Parse(fields[7]),
-                    AttackRange = float.Parse(fields[8]),
-                    DropJellyPower = int.Parse(fields[9]),
-                };
-                enemys.Add(enemy);
-            }
-        }
-    }
-
 }
 
 [System.Serializable]
-public class Slime
+public class UnitData
 {
     public int Index;
     public string Name;
     public int Grade;
     public float HP;
-    public float AttackDamage;
+    public float MP;
+    public float AttackPower;
     public float Defense;
     public float AttackSpeed;
     public float MoveSpeed;
@@ -128,17 +72,9 @@ public class Slime
     public string KRName;
     public string DesText;
 }
+
 [System.Serializable]
-public class Enemy
+public class CommanderData
 {
-    public int Index;
-    public string Name;
-    public float HP;
-    public float AttackDamage;
-    public float Defense;
-    public float AttackSpeed;
-    public float MoveSpeed;
-    public int Class;
-    public float AttackRange;
-    public float DropJellyPower;
+
 }
