e7cb8065e0794eb5cb8e6fa11f6e0855042040f3
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-07-08 10:30:18 +0900

    Basic Unit Behaviour

---
+++ Assets/01_Scripts/Unit/Unit.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Unit
+{
+    // Properties
+    private float Hp { get; set; }
+    private float Mp { get; set; }
+    private float Damage { get; set; }
+
+    public void MoveToMousePosition()
+    {
+    }
+
+    public void Attack()
+    {
+    }
+
+    public void Die()
+    {
+    }
+
+    public void UseSkill()
+    {
+    }
+}
