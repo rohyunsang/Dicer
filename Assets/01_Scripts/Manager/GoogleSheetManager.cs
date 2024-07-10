3e58cc676ee5c8174f9e797d8373b614ee3a00e9
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-07-10 19:15:47 +0900

    Network Object

--- Assets/01_Scripts/Manager/GoogleSheetManager.cs
+++ Assets/01_Scripts/Manager/GoogleSheetManager.cs
@@ -9,8 +9,8 @@ public static class GoogleSheetManager
     public static List<UnitData> unitDatas = new List<UnitData>();
 
     // 링크 뒤 edit ~ 부분을 빼고 export?format=tsv 추가하기
-    // origin url https://docs.google.com/spreadsheets/d/1Z_awcApNODEqu---j7VFGDjjuyU5cl7IMinMXKHPII/edit?gid=0#gid=0
-    private const string unitDataURL = "https://docs.google.com/spreadsheets/d/1Z_awcApNODEqu---j7VFGDjjuyU5cl7IMinMXKHPII/export?format=tsv"; 
+    // origin url https://docs.google.com/spreadsheets/d/1Z_awcApNOODEqu---j7VFGDjjuyU5cl7IMinMXKHPII/edit?gid=0#gid=0
+    private const string unitDataURL = "https://docs.google.com/spreadsheets/d/1Z_awcApNOODEqu---j7VFGDjjuyU5cl7IMinMXKHPII/export?format=tsv"; 
     public static IEnumerator Loader()
     {
         UnityWebRequest www = UnityWebRequest.Get(unitDataURL);
@@ -36,13 +36,13 @@ public static class GoogleSheetManager
                     Index = int.Parse(fields[0]),
                     Name = fields[1],
                     Grade = int.Parse(fields[2]),
-                    HP = float.Parse(fields[3]),
-                    MP = float.Parse(fields[4]),
-                    AttackPower = float.Parse(fields[5]),
-                    Defense = float.Parse(fields[6]),
-                    AttackSpeed = float.Parse(fields[7]),
-                    MoveSpeed = float.Parse(fields[8]),
-                    Class = int.Parse(fields[9]),
+                    Class = int.Parse(fields[3]),
+                    HP = float.Parse(fields[4]),
+                    MP = float.Parse(fields[5]),
+                    AttackPower = float.Parse(fields[6]),
+                    Defense = float.Parse(fields[7]),
+                    AttackSpeed = float.Parse(fields[8]),
+                    MoveSpeed = float.Parse(fields[9]),
                     AttackRange = float.Parse(fields[10]),
                     Cost = int.Parse(fields[11]),
                     KRName = fields[12],
@@ -60,13 +60,13 @@ public class UnitData
     public int Index;
     public string Name;
     public int Grade;
+    public int Class;
     public float HP;
     public float MP;
     public float AttackPower;
     public float Defense;
     public float AttackSpeed;
     public float MoveSpeed;
-    public int Class;
     public float AttackRange;
     public int Cost;
     public string KRName;
